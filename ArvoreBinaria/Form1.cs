a2a497b9f8f7f2b32c2aa1f664f6dffc073fa862
Marcelo Ponciano <sorcerer.kletian@gmail.com>
2018-05-31 18:51:14 -0300

    Inclusao de nodos, exibicao pre ordem e pos ordem

---
+++ ArvoreBinaria/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ArvoreBinaria.Node;
+
+namespace ArvoreBinaria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insert(12);
+            insert(4);
+            insert(8);
+            insert(2);
+            insert(6);
+            insert(16);
+            textBox1.Text = preOrdem(root);
+            textBox2.Text = posOrdem(root);
+            preOrdem(root);
+        }
+    }
+}
---
+++ ArvoreBinaria/Node.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreBinaria
+{
+    class Node
+    {
+        Node nodeLeft;
+        Node nodeRight;
+        Node pai;
+        int valor { get; set; }
+        public static Node root;
+
+        public Node(int v)
+        {
+            this.valor = v;
+        }
+        public Node(int v, Node pai)
+        {
+            this.valor = v;
+            this.pai = pai;
+        }
+
+        public static void insert(int v)
+        {
+            insert(root, v);
+        }
+
+        public static void insert(Node node, int v)
+        {
+            if (node == null)
+            {
+                root = new Node(v);
+            }
+            else
+            {
+                if (v < node.valor)
+                {
+                    if (node.nodeLeft != null)
+                    {
+                        insert(node.nodeLeft, v);
+                    }
+                    else
+                    {
+                        node.nodeLeft = new Node(v, node);
+                        Console.WriteLine("inserido " + v + " a esquerda de " + node.valor);
+                        Console.WriteLine("Pai de " + v + ": " + node.valor);
+                    }
+                }
+                else
+                {
+                    if (node.nodeRight != null)
+                    {
+                        insert(node.nodeRight, v);
+                    }
+                    else
+                    {
+                        node.nodeRight = new Node(v, node);
+                        Console.WriteLine("inserido " + v + " a direita de " + node.valor);
+                        Console.WriteLine("Pai de " + v + ": " + node.valor);
+                    }
+                }
+            }
+        }
+
+        public static string preOrdem(Node node)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (node != null)
+            { 
+                sb.Append(node.valor);
+                sb.Append(", ");
+                sb.Append(preOrdem(node.nodeLeft));
+                sb.Append(preOrdem(node.nodeRight));               
+            }
+            return sb.ToString();
+        }
+
+        public static string posOrdem(Node node)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (node != null)
+            {
+                sb.Append(posOrdem(node.nodeLeft));
+                sb.Append(posOrdem(node.nodeRight));                
+                sb.Append(node.valor);
+                sb.Append(", ");    
+            }
+            return sb.ToString();
+        }
+
+
+
+    }
+}
